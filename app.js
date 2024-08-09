867c6c396082a2bd5ea7e1099edcdd0f559ff511
amrrdev <amrrdev@gmail.com>
2024-08-09 15:06:22 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from "express";
+import fileUpload from "express-fileupload";
+const app = express();
+
+app.use(express.json());
+app.use(fileUpload({ useTempFiles: true }));
+
+export default app;
---
+++ server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+import app from "./app.js";
+import mongoose from "mongoose";
+
+const DB = process.env.DATABASE_STRING.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB)
+    .then(() => console.log(`DB Connected Successfully`))
+    .catch((err) =>
+        console.log(`Error while connecting with DB -> ${err?.message}`)
+    );
+
+const PORT = process.env.PORT || 9090;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
