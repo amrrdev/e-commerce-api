46586bebeea556876c226aacc9924511b32372d0
amrrdev <amrrdev@gmail.com>
2024-08-09 18:22:28 +0300

    Add custom class for handling errors

---
+++ utils/appError.js
@@ -0,0 +1,11 @@
+export default class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+        this.status = String(statusCode).startsWith("4") ? "fail" : "error";
+        // create a .stack property on the error object,
+        // which provides a string representation
+        // of the point in the code at which the Error was instantiated.
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
