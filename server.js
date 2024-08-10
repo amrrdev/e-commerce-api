8799f257fb56d7d9af792a3b66bb0926e5a8b6f9
amrrdev <amrrdev@gmail.com>
2024-08-11 00:56:51 +0300

    Fix DB connections

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ import app from "./app.js";
 import mongoose from "mongoose";
 
 const DB = process.env.DATABASE_STRING.replace(
-    "<PASSWORD>",
-    process.env.DATABASE_PASSWORD
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
 );
 
 mongoose
-    .connect(DB)
-    .then(() => console.log(`DB Connected Successfully`))
-    .catch((err) =>
-        console.log(`Error while connecting with DB -> ${err?.message}`)
-    );
+  .connect(DB)
+  .then(() => console.log(`DB Connected Successfully`))
+  .catch((err) =>
+    console.log(`Error while connecting with DB -> ${err.message}`)
+  );
 
 const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
