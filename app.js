18ec3490e5a002714e573eacf3adad3393030988
amrrdev <amrrdev@gmail.com>
2024-08-11 00:54:42 +0300

    Implemented cookie-parser middleware for handling cookiess

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
+import globalErrorHandlerMiddleware from "./middlewares/globalErrorHandler.js";
+import notFoundMiddleware from "./middlewares/notFoundHandler.js";
+import authRouter from "./routers/authRoutes.js";
+
 import express from "express";
-import fileUpload from "express-fileupload";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
 const app = express();
 
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
 app.use(express.json());
-app.use(fileUpload({ useTempFiles: true }));
+
+// SIGNING THE COOKIE
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.get("/", (req, res) => {
+  // when i signed the cookie , the cookie will not available in req.cookie
+  // instead will be available in req.signedCookies
+
+  console.log(req.signedCookies);
+  res.send("<h1>e-commerce</h1>");
+});
+
+app.use("/api/v1/auth", authRouter);
+
+app.use(notFoundMiddleware);
+
+app.use(globalErrorHandlerMiddleware);
 
 export default app;
