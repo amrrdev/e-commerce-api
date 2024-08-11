7340baf6d0fd1df6e0bd5e0583484858d790a934
amrrdev <amrrdev@gmail.com>
2024-08-11 22:43:48 +0300

    feat(auth): add login and logout functionality with JWT
    - Implemented login function to authenticate users and attach JWT to response
    - Added logout function to clear JWT cookie and log out users
    - Improved authentication flow with JWT handling for login and logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,9 +18,9 @@ export const register = catchAsync(async (req, res, next) => {
     role,
   });
 
-  const payload = { name: user.name, userId: user._id, role: user.role };
+  // const payload = { name: user.name, userId: user._id, role: user.role };
 
-  JWT.attachCookiesToResponse(res, payload);
+  JWT.attachCookiesToResponse(res, JWT.createJWTPayload(user));
 
   res.status(StatusCode.CREATED).json({
     status: "success",
@@ -28,9 +28,36 @@ export const register = catchAsync(async (req, res, next) => {
   });
 });
 
-export const login = catchAsync(async (req, res, next) => {});
-export const logout = catchAsync(async (req, res, next) => {});
-export const getUsers = async (req, res, next) => {
-  const users = await User.find({});
-  res.status(StatusCode.OK).json({ users });
-};
+export const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return next(
+      new AppError("Missing email or password!", StatusCode.BAD_REQUEST)
+    );
+
+  const user = await User.findOne({ email }).select("+password -__v");
+  if (!user || !(await user.comparePasswords(password, user.password))) {
+    return next(
+      new AppError("Invalid Email or Password!", StatusCode.UNAUTHORIZED)
+    );
+  }
+
+  // const payload = { name: user.name, userId: user._id, role: user.role };
+
+  JWT.attachCookiesToResponse(res, JWT.createJWTPayload(user));
+
+  user.password = undefined;
+
+  res.status(StatusCode.OK).json({
+    status: "success",
+    data: { user },
+  });
+});
+
+export const logout = catchAsync(async (req, res, next) => {
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCode.OK).json({ message: "user logged out" });
+});
