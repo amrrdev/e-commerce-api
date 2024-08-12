6a20efaecada03040aaa7e548af4a860bdb30a86
amrrdev <amrrdev@gmail.com>
2024-08-12 20:42:05 +0300

    feat: set up product routes with authentication and admin authorization
    - Configured public routes for retrieving all products and a single product.
    - Added routes requiring authentication for product creation and image upload.
    - Integrated admin authorization for creating products and uploading images.
    - Marked /uploadImage route as unstable and in need of further testing.
    - Implemented route for updating and deleting products by ID.
    - Organized routes with appropriate middleware for authentication and authorization.

---
+++ routers/productRoutes.js
@@ -0,0 +1,35 @@
+import * as productController from "./../controllers/productController.js";
+import * as authentication from "./../middlewares/authentication.js";
+
+import express from "express";
+
+const router = express.Router();
+
+// Public routes
+router.route("/").get(productController.getAllProducts);
+router.route("/:id").get(productController.getSingleProduct);
+
+// Routes that require authentication
+router.use(authentication.isAuthenticated);
+
+// Routes that require admin authorization
+router.use(authentication.authorizePermission("admin"));
+
+router.route("/").post(productController.createProduct);
+
+// upload images
+router.post("/uploadImage", productController.uploadImage);
+
+// NOTE: This route is currently unstable and may require further testing and improvements.
+router.post(
+  "/:id/uploadImage",
+  productController.uploadImage,
+  productController.updateProduct
+);
+
+router
+  .route("/:id")
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
+
+export default router;
